c53f4679944957401d77887cb06b48b0b97c4a35
Joshua Oppenheim <josh@Joshuas-MacBook-Pro.local>
2019-12-16 14:03:25 -0500

    got all processes up

--- index.js
+++ index.js
@@ -1,7 +1,44 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.json());
+
+let tasks = [
+	{
+		id: 001,
+		task: 'eat yogurt',
+	},
+	{
+		id: 002,
+		task: 'make a sandwich',
+	},
+];
+
 app.get('/', (req, res) => res.send('Hello World!'));
 
+app.get('/tasks', (req, res) => res.send(tasks));
+
+app.get('/task/:id', (req, res) =>
+	res.send(tasks.find(task => task.id === +req.params.id))
+);
+
+app.put('/updateTask', (req, res) => {
+	let index = tasks.findIndex(task => task.id === req.body.id);
+	if (index > -1) tasks.splice(index, 1, req.body);
+	res.send(tasks);
+});
+
+app.post('/task', (req, res) => {
+	tasks.push(req.body);
+	res.send(tasks);
+});
+
+app.delete('/task/:id', (req, res) => {
+	let index = tasks.findIndex(task => task.id === +req.params.id);
+	if (index > -1) tasks.splice(index, 1);
+	res.send(tasks);
+});
+// To do
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
