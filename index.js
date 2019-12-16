641d6843f8050ea7843f4d03f0aa2cc95a8a9980
Joshua Oppenheim <josh@Joshuas-MacBook-Pro.local>
2019-12-16 15:30:49 -0500

    added sqlite module

--- index.js
+++ index.js
@@ -1,44 +1,19 @@
+// express module
 const express = require('express');
+//body parser allows for having the request body parsed for us
 const bodyParser = require('body-parser');
+const taskRouter = require('./task.router');
 const app = express();
 const port = 3000;
 
+// specifying I am expecting the request body to be json format
 app.use(bodyParser.json());
-
-let tasks = [
-	{
-		id: 001,
-		task: 'eat yogurt',
-	},
-	{
-		id: 002,
-		task: 'make a sandwich',
-	},
-];
-
-app.get('/', (req, res) => res.send('Hello World!'));
-
-app.get('/tasks', (req, res) => res.send(tasks));
-
-app.get('/task/:id', (req, res) =>
-	res.send(tasks.find(task => task.id === +req.params.id))
+app.use('/api/v1', taskRouter);
+app.get('/healthcheck', (req, res) =>
+	res.send({
+		uptime: process.uptime(),
+		message: 'OK',
+		timestamp: Date.now(),
+	})
 );
-
-app.put('/updateTask', (req, res) => {
-	let index = tasks.findIndex(task => task.id === req.body.id);
-	if (index > -1) tasks.splice(index, 1, req.body);
-	res.send(tasks);
-});
-
-app.post('/task', (req, res) => {
-	tasks.push(req.body);
-	res.send(tasks);
-});
-
-app.delete('/task/:id', (req, res) => {
-	let index = tasks.findIndex(task => task.id === +req.params.id);
-	if (index > -1) tasks.splice(index, 1);
-	res.send(tasks);
-});
-// To do
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
